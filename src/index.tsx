2ace27d180c28991e81935fcf15cc2b6f015577e
mishra_mridul <mridulmishra2991@gmail.com>
2022-02-19 22:28:24 +0530

    fix: updated routes to config dir

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRoutes from './routes';
+import AppRoutes from './config/routes';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
