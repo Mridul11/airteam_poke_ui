bbf2d74341699277fdd87f0b9e1faf4f0c375bf2
mishra_mridul <mridulmishra2991@gmail.com>
2022-02-20 22:26:39 +0530

    fix: updated service worker file

--- public/sw.js
+++ public/sw.js
@@ -15,28 +15,36 @@ self.addEventListener('install', event => {
     )
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(fromCache(event.request)) 
-    
-    // update cache
-    event.waitUntil(update(event.request));
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+            return response;
+            }
+
+            return fetch(event.request).then(
+            function(response) {
+                // Check if we received a valid response
+                if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+                }
+
+                // IMPORTANT: Clone the response. A response is a stream
+                // and because we want the browser to consume the response
+                // as well as the cache consuming the response, we need
+                // to clone it so we have two streams.
+                var responseToCache = response.clone();
+
+                caches.open(CACHE_NAME)
+                .then(function(cache) {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return response;
+            }
+            );
+        })
+    );
 });
-
-
-/**
- * 
- * Helper methods
- */
-function fromCache(request) {
-    return caches.open(CACHE_NAME).then(cache => {
-        return cache.match(request);
-    });
-}
-
-function update(request) {
-    caches.open(CACHE_NAME).then( cache => {
-        fetch(request).then( response => {
-        cache.put(request, response)
-        });
-    });
-}
